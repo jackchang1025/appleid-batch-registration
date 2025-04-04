ac0720dd767d500b0df24018f8a9f8c68b6e05ab
weijiajia <123456@qq.com>
2025-04-04 16:47:52 +0800

    优化 Apple ID 注册命令
    - 更新国家选项，支持更多国家的输入格式
    - 移除不必要的依赖注入，简化代码逻辑
    - 引入国家语言服务，增强国家处理能力

--- app/Console/Commands/AppleIdRegistration.php
+++ app/Console/Commands/AppleIdRegistration.php
@@ -2,31 +2,21 @@
 
 namespace App\Console\Commands;
 
-use App\Models\ProxyConfiguration;
 use App\Services\AppleClientIdService;
 use App\Services\Exception\RegistrationException;
-use App\Services\Helper\Helper;
-use DateTime;
-use DateTimeZone;
 use Illuminate\Console\Command;
-use Illuminate\Support\Carbon;
 use JsonException;
 use Propaganistas\LaravelPhone\Exceptions\NumberFormatException;
 use Random\RandomException;
 use Saloon\Exceptions\Request\ClientException;
 use Saloon\Exceptions\Request\FatalRequestException;
 use Saloon\Exceptions\Request\RequestException;
-use Saloon\Exceptions\Request\Statuses\ServiceUnavailableException;
-use Weijiajia\DecryptVerificationCode\Exception\DecryptCloudCodeException;
 use Weijiajia\SaloonphpAppleClient\Exception\AccountAlreadyExistsException;
 use Weijiajia\SaloonphpAppleClient\Exception\MaxRetryAttemptsException;
-use Weijiajia\SaloonphpAppleClient\Exception\Phone\PhoneException;
-use Illuminate\Support\Facades\Cache;
 use App\Models\Email;
-use Illuminate\Contracts\Cache\Lock;
 use App\Services\AppleId\AppleIdBatchRegistration as AppleIdBatchRegistrationService;
 use App\Enums\EmailStatus;
-
+use App\Services\CountryLanguageService;
 
 class AppleIdRegistration extends Command
 {
@@ -35,7 +25,7 @@ class AppleIdRegistration extends Command
      *
      * @var string
      */
-    protected $signature = 'app:apple-id-registration {email} {--country=USA}';
+    protected $signature = "app:apple-id-registration {email} {--C|country=CAN}";
 
     /**
      * The console command description.
@@ -59,7 +49,7 @@ class AppleIdRegistration extends Command
      * @throws AccountAlreadyExistsException
      * @throws MaxRetryAttemptsException
      */
-    public function handle(AppleClientIdService $appleClientIdService): void
+    public function handle(): void
     {
         $email = Email::where('email', $this->argument('email'))
          ->whereIn('status', [EmailStatus::AVAILABLE, EmailStatus::FAILED])
@@ -67,7 +57,7 @@ class AppleIdRegistration extends Command
 
         $appleIdBatchRegistration = app(AppleIdBatchRegistrationService::class);
 
-        $appleIdBatchRegistration->run($email,strtoupper($this->option('country')));
+        $appleIdBatchRegistration->run($email,CountryLanguageService::make($this->option('country')));
 
     }
 
