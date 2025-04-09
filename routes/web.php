43d83ca37204d27ea9a8e0d6dac5952664a71fd7
weijiajia <123456@qq.com>
2025-04-09 16:13:37 +0800

    feat(ip-info): 新增 IpInfoController 以获取 IP 信息
    - 创建 IpInfoController，提供获取客户端 IP、请求头和请求体的接口
    - 在路由中添加 /ip-info 路由，映射到 IpInfoController 的 index 方法

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\IpInfoController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ip-info', [IpInfoController::class, 'index']);
